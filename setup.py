f9a2e793ebd9b42e07d1e6d63d2e2ddcaada3a58
xaxt176 <aditya.tewari@grainger.com>
2022-02-17 11:01:13 -0600

    Added

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 __version__ = '1.0.0'
 
 setup(
-    name='pyspark-seed',
+    name='adobe-pyspark',
     version=__version__,
     packages=find_packages(),
+    author="Aditya S Tewari",
+    author_email="aditya.tewari@yahoo.com",
     install_requires=['pyspark', 'boto3']
 )
 
