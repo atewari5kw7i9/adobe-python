ad8df0b49680f0a319a300df541b47326cf7375b
xaxt176 <aditya.tewari@grainger.com>
2022-02-20 12:17:00 -0600

    Added

--- src/jobs/utils/spark_utils.py
+++ src/jobs/utils/spark_utils.py
@@ -13,24 +13,26 @@ class adobe_data:
             .csv(input_path)
 
     def select_adobe_fields(self, adobe_df):
+        print("Step1")
         select_cols = Settings.columns_select
         return adobe_df.select(*select_cols)
 
     def cast_adobe_df(self, adobe_df):
+        print("Step2")
         return adobe_df.select(split(col("product_list"), ",").alias("product_list_arr"),\
                                split(col("event_list"), ",").alias("event_list_arr"),\
                                col("event_list"), col("page_url"), col("referrer")) \
                                .drop("product_list").drop("event_list")
 
     def explode_adobe_df(self, adobe_df):
-        print("Step4")
+        print("Step3")
         adobe_df_tmp = adobe_df.select(explode(adobe_df.product_list_arr).alias("product_attributes"), adobe_df.event_list_arr, adobe_df.page_url, adobe_df.referrer)
         adobe_df_tmp = adobe_df_tmp.select(explode(adobe_df_tmp.event_list_arr).alias("events_types"), adobe_df_tmp.product_attributes, adobe_df_tmp.page_url, adobe_df_tmp.referrer)
         adobe_df_tmp = adobe_df_tmp.withColumn("events_types", col("events_types").cast("int"))
         return adobe_df_tmp
 
     def filter_adobe_df(self, adobe_df):
-        print("Step3")
+        print("Step4")
         return adobe_df.filter("events_types = 1") \
              .select("events_types", "product_attributes", "page_url", "referrer")
 
@@ -57,7 +59,11 @@ class adobe_data:
         df = adobe_df.selectExpr("cast(domain_name as string) domain_name",
                              "cast(search1 as string) search1",
                              "cast(Total_Revenue as double) Total_Revenue")
-        summary_df = df.groupBy("domain_name", "search1").sum("Total_Revenue").alias("Total_Revenue").orderBy("Total_Revenue")
+        summary_df = df.groupBy("domain_name", "search1").\
+                        sum("Total_Revenue").\
+                        withColumnRenamed("sum(Total_Revenue)", "Total_Revenue").\
+                        sort(col("Totall_Revenue").desc())
+
         return summary_df
 
     def write_out_file(self, adobe_df, s3_out_path):
